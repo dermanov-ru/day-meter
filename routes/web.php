3551efe986575a3607ee2dfee865bab0be3859ea
Mark <dev@dermanov.ru>
2026-01-02 00:28:54 +0300

    T-X Удалить лишний роут /today

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::get('/today', function () {
-    return view('today');
-})->middleware(['auth', 'verified'])->name('today');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
